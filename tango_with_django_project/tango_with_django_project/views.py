269e5390a5d3c029c54fef19ff0e2445d334cba4
Alexey Sokolov <sokolovbiotech@gmail.com>
2014-05-04 19:12:44 +0400

    module6

--- tango_with_django_project/tango_with_django_project/views.py
+++ tango_with_django_project/tango_with_django_project/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from rango.models import Category,Page
 
 def home(request):
-    return HttpResponse("This is home! And here is rango: <a href='/rango/'>Rango</a>")
+    # Obtain the context from the HTTP request.
+    context = RequestContext(request)
+
+    # Query for categories - add the list to our context dictionary.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    # The following two lines are new.
+    # We loop through each category returned, and create a URL attribute.
+    # This attribute stores an encoded URL (e.g. spaces replaced with underscores).
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
+
+    # Render the response and return to the client.
+    return render_to_response('tango/index.html', context_dict, context)
